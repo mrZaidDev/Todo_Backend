d053668a96852883a3eadcbe5ef415aa9583bee5
Zaid Khan <zaidcodingtopcoding@gmail.com>
2025-06-24 15:43:45 +0500

    careless about security

--- app.js
+++ app.js
@@ -9,17 +9,17 @@ const mongoose = require('mongoose')
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 // app.use(cors())
-require('dotenv').config();
+// require('dotenv').config();
 
 // ROUTES ...
 app.use(taskRouter)
 app.use(taskIdRouter)
 
 // Connect to MongoDB and start server
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect('mongodb+srv://ZaidKhan:mcfniN2015@node-express-projects.bn0xlg9.mongodb.net/CRUD_APP?retryWrites=true&w=majority')
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(process.env.PORT || 4000, () => {
+    app.listen(3000, () => {
       console.log('Server running');
     });
   })
